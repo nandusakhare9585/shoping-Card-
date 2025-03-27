f516e5c8afa1c93d1c9bf40de5fccf1ba2b68237
Nandu Sakhare <nandusakhare9585@gmail.com>
2025-03-27 15:35:06 +0530

    Shopping Card

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // ✅ Includes JSX & TSX files
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
